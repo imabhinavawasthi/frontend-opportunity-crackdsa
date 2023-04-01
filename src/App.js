6b449be4c2476aa2d966c237fdb0ee9fafb21f97
priyanshi-158 <priyanshidhanuka9@gmail.com>
2023-04-01 11:42:07 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //imprt pages
 import LandingPage from './pages/LandingPage';
 import All_intern from './pages/All_intern';
@@ -9,16 +9,15 @@ import MoreDetails from './pages/MoreDetails';
 import Get from './Hooks/Get';
 
 function App() {
-  const {frontend_intern,backend_intern,full_stack,loading,category,all_intern}=Get()
+  const { frontend_intern, backend_intern, full_stack, loading, category, all_intern } = Get()
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-  <Route exact path='/' element={<LandingPage frontend_intern={frontend_intern} backend_intern={backend_intern} full_stack={full_stack} loading={loading}/>} />
-  <Route exact path='/:id'  element={<MoreDetails internships={all_intern} loading={loading}/>}/> 
-  <Route exact path='/:catogery/all_intern' element={<All_intern internships={category==='full stack'?full_stack:(category==='frontend'?frontend_intern:backend_intern)}/> } />
-
-      </Routes>
+        <Routes>
+          <Route exact path='/' element={<LandingPage frontend_intern={frontend_intern} backend_intern={backend_intern} full_stack={full_stack} loading={loading} />} />
+          <Route exact path='/:id' element={<MoreDetails internships={all_intern} loading={loading} />} />
+          <Route exact path='/:catogery/all_intern' element={<All_intern internships={category === 'full stack' ? full_stack : (category === 'frontend' ? frontend_intern : backend_intern)} />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );

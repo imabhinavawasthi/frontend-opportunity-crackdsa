fd100e2d2ebe7ab923f74cea6ba852dfd7180a9e
naren123we <narendramohanpathak@gmail.com>
2023-04-18 15:03:47 +0530

    MoreDetails2 added

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -15,13 +15,13 @@ const Card2 = ({ data }) => {
           <div>
             <dt className="sr-only">Company</dt>
 
-            <dd className="text-sm text-gray-500">{data.company}</dd>
+            <dd className="text-sm text-gray-500 capitalize">{data.company}</dd>
           </div>
 
           <div>
             <dt className="sr-only">Title</dt>
 
-            <dd className="font-medium">{data.title}</dd>
+            <dd className="font-medium capitalize">{data.title}</dd>
           </div>
         </dl>
 
@@ -39,7 +39,7 @@ const Card2 = ({ data }) => {
             <div className="mt-1.5 sm:mt-0">
               <p className="text-gray-500">Location</p>
 
-              <p className="font-medium">{data.location}</p>
+              <p className="font-medium capitalize">{data.location}</p>
             </div>
           </div>
 
@@ -53,9 +53,9 @@ const Card2 = ({ data }) => {
               <path d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z" />
             </svg>
             <div className="mt-1.5 sm:mt-0">
-              <p className="text-gray-500">Last Date</p>
+              <p className="text-gray-500 capitalize">Last Date</p>
 
-              <p className="font-medium">{data.lastdate.slice(0, 10)}</p>
+              <p className="font-medium capitalize">{data.lastdate.slice(0, 10)}</p>
             </div>
           </div>
 
@@ -72,7 +72,7 @@ const Card2 = ({ data }) => {
             <div className="mt-1.5 sm:mt-0">
               <p className="text-gray-500">Duration</p>
 
-              <p className="font-medium">{data.duration}</p>
+              <p className="font-medium capitalize">{data.duration}</p>
             </div>
           </div>
         </div>

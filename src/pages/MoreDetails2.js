06bb1d12a3942a1e6d77e6c4cbb85680921ed242
priyanshi-158 <priyanshidhanuka9@gmail.com>
2023-04-24 19:38:23 +0530

    Updated

--- src/pages/MoreDetails2.js
+++ src/pages/MoreDetails2.js
@@ -781,14 +781,14 @@ const MoreDetails2 = () => {
             </footer> */}
             {/* <a href="https://github.com/mertJF/tailblocks" class="rounded-full w-12 h-12 bg-gray-100 fixed bottom-0 right-0 flex items-center justify-center text-gray-800 mr-8 mb-8 shadow-sm border-gray-300 border" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-            </svg></a>
+            </svg></a>*/}
             {token && <div className='p-3'>
      <Deleate token={token} id={data._id}/> 
      <button className='bg-[#2FCDFF] border-[#0f94bdc7] border border-solid rounded-xl px-14 hover:bg-[#1583a5] my-2 py-1 font-Poppins font-semibold max-lg:w-[100%]  active:scale-105' onClick={()=>{uptodate()}}>Update</button>
      {update&& <Update data={data} token={token} uptodate={uptodate} id={id} setpop={setpop}/>}
      </div>}
 
-     {popup!==''&&<h1 className=' text-center text-xl  text-indigo-600 p-2'>{popup}</h1>} */}
+     {popup!==''&&<h1 className=' text-center text-xl  text-indigo-600 p-2'>{popup}</h1>} 
         </div>
     )
 }

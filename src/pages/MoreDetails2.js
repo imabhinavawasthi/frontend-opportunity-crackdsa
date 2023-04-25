1e80c46e53f01f6ecf73535b4cf598a7d72e1171
priyanshi-158 <priyanshidhanuka9@gmail.com>
2023-04-25 22:27:58 +0530

    Updated More details

--- src/pages/MoreDetails2.js
+++ src/pages/MoreDetails2.js
@@ -102,31 +102,35 @@ const MoreDetails2 = () => {
             <section className="text-gray-700 body-font border-t border-gray-200">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-col text-center w-full mb-20">
-                        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Master Cleanse Reliac Heirloom</h1>
+                        {/* <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2> */}
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Skills Required</h1>
                     </div>
                     <div className="flex flex-wrap -m-4">
-                        <div className="p-4 md:w-1/3">
-                            <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+
+                    {data.skills.map((detail)=>{
+          return  <div className="p-4 md:w-1/3">
+                            <div className="flex rounded-lg bg-gray-100 p-8 flex-col">
                                 <div className="flex items-center mb-3">
                                     <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
                                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                                             <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                                         </svg>
                                     </div>
-                                    <h2 className="text-gray-900 text-lg title-font font-medium">Shooting Stars</h2>
+                                    <h2 className="text-gray-900 text-lg title-font capitalize font-medium">{detail}</h2>
                                 </div>
-                                <div className="flex-grow">
+                                {/* <div className="flex-grow">
                                     <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
                                     <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                                         </svg>
                                     </a>
-                                </div>
+                                </div> */}
                             </div>
                         </div>
-                        <div className="p-4 md:w-1/3">
+        })}
+                        
+                        {/* <div className="p-4 md:w-1/3">
                             <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                                 <div className="flex items-center mb-3">
                                     <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
@@ -168,65 +172,68 @@ const MoreDetails2 = () => {
                                     </a>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </section>
             <section className="text-gray-700 body-font border-t border-gray-200">
                 <div className="container px-5 py-24 mx-auto flex flex-wrap">
                     <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
-                        <img alt="feature" className="object-cover object-center h-full w-full" src="https://dummyimage.com/600x600/edf2f7/a5afbd" />
+                        <img alt="feature" className="object-cover object-center " src="https://dummyimage.com/600x600/edf2f7/a5afbd" />
                     </div>
-                    <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+                    <div className="flex flex-col flex-wrap justify-center lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
                         <div className="flex flex-col mb-10 lg:items-start items-center">
                             <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                {/* <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
                                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                                </svg>
+                                </svg> */}
+                                <FaMoneyBill/>
                             </div>
                             <div className="flex-grow">
-                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                                <a classNameName="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Stipend</h2>
+                                <p className="leading-relaxed text-base">{data.stipend}</p>
+                                {/* <a classNameName="mt-3 text-indigo-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
-                                </a>
+                                </a> */}
                             </div>
                         </div>
                         <div className="flex flex-col mb-10 lg:items-start items-center">
                             <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                {/* <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
                                     <circle cx="6" cy="6" r="3"></circle>
                                     <circle cx="6" cy="18" r="3"></circle>
                                     <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                                </svg>
+                                </svg> */}
+                                <BsCalendarEvent/>
                             </div>
                             <div className="flex-grow">
-                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
-                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                                <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Deadlines :</h2>
+                                <p className="leading-relaxed text-base">Last date to apply: {data.lastdate.slice(0,10)}</p>
+                                {/* <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
-                                </a>
+                                </a> */}
                             </div>
                         </div>
                         <div className="flex flex-col mb-10 lg:items-start items-center">
                             <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
-                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                {/* <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
                                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                                     <circle cx="12" cy="7" r="4"></circle>
-                                </svg>
+                                </svg> */}
+                                <HiLocationMarker/>
                             </div>
                             <div className="flex-grow">
-                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-                                <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Location</h2>
+                                <p className="leading-relaxed text-base">{data.location}</p>
+                                {/* <a className="mt-3 text-indigo-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
-                                </a>
+                                </a> */}
                             </div>
                         </div>
                     </div>
@@ -235,22 +242,26 @@ const MoreDetails2 = () => {
             <section className="text-gray-700 body-font border-t border-gray-200">
                 <div className="container px-5 py-24 mx-auto">
                     <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
-                        <p className="lg:w-1/2 w-full leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Requirements</h1>
+                        {/* <p className="lg:w-1/2 w-full leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p> */}
                     </div>
                     <div className="flex flex-wrap -m-4">
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
+                    {data.requirements.map((detail)=>{
+          return  <div className="xl:w-1/3 md:w-1/2 p-4">
                             <div className="border border-gray-300 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
                                         <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                                     </svg>
                                 </div>
-                                <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
-                                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                                <h2 className="text-lg text-gray-900 font-medium title-font mb-2">{detail}</h2>
+                                {/* <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p> */}
                             </div>
                         </div>
-                        <div className="xl:w-1/3 md:w-1/2 p-4">
+        
+        })}
+                       
+                        {/* <div className="xl:w-1/3 md:w-1/2 p-4">
                             <div className="border border-gray-300 p-6 rounded-lg">
                                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">
@@ -307,7 +318,7 @@ const MoreDetails2 = () => {
                                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">Ramona Falls</h2>
                                 <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                     <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                     <a href={data.url} target='_blank' rel="noreferrer">  Apply Now</a>

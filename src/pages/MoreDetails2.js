bc15090f6b30437d9d6fb654c60c8fb79a9d37df
utsav82 <utsa.yadav.94@gmail.com>
2023-04-26 14:34:57 +0530

    minor changes

--- src/pages/MoreDetails2.js
+++ src/pages/MoreDetails2.js
@@ -177,11 +177,11 @@ const MoreDetails2 = () => {
                 </div>
             </section>
             <section className="text-gray-700 body-font border-t border-gray-200">
-                <div className="container px-5 py-24 mx-auto flex flex-wrap">
-                    <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
-                        <img alt="feature" className="object-cover object-center " src="https://dummyimage.com/600x600/edf2f7/a5afbd" />
+                <div className="container px-5 py-24 mx-auto flex flex-wrap justify-center">
+                    <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden flex justify-center">
+                        <img alt="feature" className="object-cover object-center " src={"https://opportunity.run-ap-south1.goorm.site" + data.image} />
                     </div>
-                    <div className="flex flex-col flex-wrap justify-center lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+                    <div className="flex flex-col flex-wrap justify-center  lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
                         <div className="flex flex-col mb-10 lg:items-start items-center">
                             <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5">
                                 {/* <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="w-6 h-6" viewBox="0 0 24 24">

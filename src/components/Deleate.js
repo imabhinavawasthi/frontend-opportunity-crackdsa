2811e18122603e8f9d1a9b72c31802cb9cad989c
utsav82 <utsa.yadav.94@gmail.com>
2023-05-12 23:18:45 +0530

    added confirm to delete

--- src/components/Deleate.js
+++ src/components/Deleate.js
@@ -1,37 +1,55 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Deleate = ({token,id}) => {
+const Deleate = ({ token, id }) => {
+  const navigate = useNavigate();
+  const [deleting,setDeleting] = useState(false);
 
- 
-  const navigate=useNavigate()
+  async function Del() {
+    try {
+      const options = {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
 
+      const res = await fetch(
+        "https://opportunity.run-ap-south1.goorm.site/jobs/" + id,
+        options
+      );
+      const data = await res.json();
 
-    async function Del(){
-        try{
-            const options={
-                method: 'DELETE',
-                headers: {
-                          'Authorization':`Bearer ${token}`
-              },
-             
-            }
-
-        const res=await fetch('https://opportunity.run-ap-south1.goorm.site/jobs/'+id,options)
-        const data =await res.json()
-       
-        navigate('/')
-        }catch(err){
-            console.log(err.message)
-        }
+      navigate("/");
+    } catch (err) {
+      console.log(err.message);
     }
+  }
   return (
-    <div className=''>
-   <button onClick={()=>{Del()}} className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
-          Delete
+    <div className="">
+      <button
+        onClick={() => {
+          setDeleting(!deleting);
+        }}
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        Delete
+      </button>
+      {deleting && (
+        <div className="absolute pt-2.5">
+          Press Confirm to delete
+          <button
+            onClick={() => {
+              Del();
+            }}
+            className="text-white bg-indigo-500 border-0 mx-5 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            Confirm
           </button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Deleate
+export default Deleate;
